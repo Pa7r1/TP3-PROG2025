78b1301a6873b6a0717fa2b7945177a1189ed46f
Pa7r1 <valentincarpio32@gmail.com>
2025-06-26 14:24:00 -0300

    final script ejercicio1

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -1,15 +1,38 @@
+const contenedor = document.getElementById("contenedor-mayor");
 const formulario = document.getElementById("formulario");
 const n1 = document.getElementById("n1");
 const n2 = document.getElementById("n2");
-const contenedor = document.getElementById("contenedor-mayor");
-selector = document.getElementById("operaciones");
+const selector = document.getElementById("operaciones");
+const boton = document.getElementById("btn");
+
+function verificarDivision() {
+  if (selector.value === "division" && (n1.value == 0 || n2.value == 0)) {
+    boton.disabled = true;
+  } else {
+    boton.disabled = false;
+  }
+}
+
+n1.addEventListener("input", verificarDivision);
+n2.addEventListener("input", verificarDivision);
+selector.addEventListener("change", verificarDivision);
+
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
+
   numero1 = Number(n1.value);
   numero2 = Number(n2.value);
   operacion = selector.value;
   resultado = Number;
 
+  if (!numero1 || !numero2) {
+    let span = document.createElement("span");
+    span.innerHTML = "ingrese valores";
+    let br = document.createElement("br");
+    contenedor.appendChild(span);
+    contenedor.appendChild(br);
+    return;
+  }
   switch (operacion) {
     case "suma":
       resultado = numero1 + numero2;
@@ -28,9 +51,11 @@ formulario.addEventListener("submit", (e) => {
       resultado = numero1 / numero2;
       break;
   }
+
   let span = document.createElement("span");
   span.innerHTML = `Resultado de la ${operacion} es: ${resultado}`;
   let br = document.createElement("br");
+
   contenedor.appendChild(span);
   contenedor.appendChild(br);
 });
